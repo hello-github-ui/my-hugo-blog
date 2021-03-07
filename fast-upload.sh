e86435648fa448189f9c54f92da158a1b54fd4b2
latin-xiao-mao <latinxiaomao@gmail.com>
2021-03-07 23:45:48 +0800

    update content on 2021-03-07 23:45:48

--- fast-upload.sh
+++ fast-upload.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-time1=$(date)
-echo "欢迎您于 ${time1} 使用快速上传源码脚本"
+
+echo "欢迎您于 ${date} 使用快速上传源码脚本"
 
 git add .
 
-git commit -m 'update content on `${time1}`'
+git commit -m "update content on `date +'%Y-%m-%d %H:%M:%S'`"
 
 git push origin master
